d84db63f4cc35a60302393a581a28cc7850394d1
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-05-27 20:36:11 +0530

    adt: adds queue  linked list implementation

--- adt/queue.go
+++ adt/queue.go
@@ -1,6 +1,8 @@
 package adt
 
-import "errors"
+import (
+	"errors"
+)
 
 // Queue - rear inserts and front removals
 
@@ -85,4 +87,71 @@ func (q *DynQueue) Peek() (int, error) {
 		return 0, errors.New("Cannot Peak on empty queue")
 	}
 	return q.arr[0], nil
+}
+
+// LinkedQueue implements queue as doubly linked list
+type LinkedQueue struct {
+	head *QueueItem
+	size int
+}
+
+// QueueItem refers to individual queue item.
+type QueueItem struct {
+	data int
+	prev *QueueItem
+	next *QueueItem
+}
+
+// Enqueue inserts elements to the queue in rear position
+func (q *LinkedQueue) Enqueue(element int) error {
+	newItem := &QueueItem{data: element}
+	if q.size == 0 {
+		q.head = newItem
+		q.head.next = q.head
+		q.head.prev = q.head
+	} else {
+		newItem.prev = q.head.prev
+		q.head.prev.next = newItem
+
+		newItem.next = q.head
+		q.head.prev = newItem
+	}
+	q.size++
+	return nil
+}
+
+// Dequeue removes elements from the queue in the front position
+func (q *LinkedQueue) Dequeue() (int, error) {
+	if q.size == 0 {
+		return 0, errors.New("Queue underflow")
+	}
+
+	var element int
+	if q.size == 1 {
+		element = q.head.data
+		q.head = nil
+	} else {
+		element = q.head.data
+		toRemove := q.head
+
+		q.head = toRemove.next
+		q.head.prev = toRemove.prev
+		q.head.prev.next = q.head
+	}
+
+	q.size--
+	return element, nil
+}
+
+// Peek returns element at the front
+func (q *LinkedQueue) Peek() (int, error) {
+	if q.head == nil {
+		return 0, errors.New("Queue underflow")
+	}
+	return q.head.data, nil
+}
+
+// Size returns size of the queue
+func (q *LinkedQueue) Size() int {
+	return q.size
 }
--- adt/queue_test.go
+++ adt/queue_test.go
@@ -1,6 +1,8 @@
 package adt
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFixedArrayQueueImplementation(t *testing.T) {
 	var queue = &ArrQueue{}
@@ -145,6 +147,116 @@ func TestDynamicArrayQueueImplementation(t *testing.T) {
 	if front != 1 {
 		t.Error("Expected 1, got ", front)
 	}
+	front, err = queue.Dequeue()
+	if err != nil {
+		t.Error("Expected dequeue to happen without error, but got", err)
+	}
+	if front != 2 {
+		t.Error("Expected 2, got ", front)
+	}
+
+	// Enqueue Two Elements
+	err = queue.Enqueue(6)
+	if err != nil {
+		t.Error("Expects to insert element in queue, but got error ", err)
+	}
+	err = queue.Enqueue(7)
+	if err != nil {
+		t.Error("Expects to insert element in queue, but got error ", err)
+	}
+
+	front, err = queue.Peek()
+	if err != nil {
+		t.Error("Expects peek to happen without error, but got", err)
+	}
+	if front != 3 {
+		t.Error("Expected 3 to be at front, but got", front)
+	}
+
+	// Dequeue 4 Elements
+	front, err = queue.Dequeue()
+	if err != nil {
+		t.Error("Expected dequeue to happen without error, but got", err)
+	}
+	if front != 3 {
+		t.Error("Expected 1, got ", front)
+	}
+	front, err = queue.Dequeue()
+	if err != nil {
+		t.Error("Expected dequeue to happen without error, but got", err)
+	}
+	if front != 4 {
+		t.Error("Expected 4, got ", front)
+	}
+	front, err = queue.Dequeue()
+	if err != nil {
+		t.Error("Expected dequeue to happen without error, but got", err)
+	}
+	if front != 5 {
+		t.Error("Expected 5, got ", front)
+	}
+	front, err = queue.Dequeue()
+	if err != nil {
+		t.Error("Expected dequeue to happen without error, but got", err)
+	}
+	if front != 6 {
+		t.Error("Expected 6, got ", front)
+	}
+
+	front, err = queue.Peek()
+	if err != nil {
+		t.Error("Expects peek to happen without error, but got", err)
+	}
+	if front != 7 {
+		t.Error("Expected 3 to be at front, but got", front)
+	}
+
+	// Empty the queue
+	front, err = queue.Dequeue()
+	if err != nil {
+		t.Error("Expected dequeue to happen without error, but got", err)
+	}
+	if front != 7 {
+		t.Error("Expected 7, got ", front)
+	}
+
+	_, err = queue.Dequeue()
+	if err == nil {
+		t.Error("Expects Queue underflow error")
+	}
+}
+
+func TestLinkedListQueueImplementation(t *testing.T) {
+	var queue = &LinkedQueue{}
+
+	_, err := queue.Dequeue()
+	if err == nil {
+		t.Error("Expects Queue underflow error")
+	}
+
+	_, err = queue.Peek()
+	if err == nil {
+		t.Error("Expects error on reading empty queue")
+	}
+
+	err = queue.Enqueue(1)
+	if err != nil {
+		t.Error("Expects to insert element in queue, but got error ", err)
+	}
+	err = queue.Enqueue(2)
+	err = queue.Enqueue(3)
+	err = queue.Enqueue(4)
+	err = queue.Enqueue(5)
+
+	// Dequeue two elements
+	front, err := queue.Dequeue()
+	if err != nil {
+		t.Error("Expected dequeue to happen without error, but got", err)
+	}
+	if front != 1 {
+		t.Error("Expected 1, got ", front)
+	}
+
 	front, err = queue.Dequeue()
 	if err != nil {
 		t.Error("Expected dequeue to happen without error, but got", err)
