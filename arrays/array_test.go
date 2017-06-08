b0283ebfc504599ed95ccec219b3ac31747c4fdf
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-06-08 14:10:31 +0530

    arrays: adds multi dimensional arrays

--- arrays/array_test.go
+++ arrays/array_test.go
@@ -1,8 +1,11 @@
 package arrays
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-func TestArray(t *testing.T) {
+func TestSingleDimensionalArray(t *testing.T) {
 	if arr[0] != 1 {
 		t.Error("Expected 1, got ", arr[0])
 	}
@@ -10,6 +13,20 @@ func TestArray(t *testing.T) {
 	for index, expected := 0, 1; index < len(arr); index, expected = index+1, expected+1 {
 		if arr[index] != expected {
 			t.Errorf("Expected %d, got %d", expected, arr[index])
+		}
+	}
+}
+
+func TestMultiDimensionalArray(t *testing.T) {
+	for rowIndex, row := range multi {
+		for colIndex := 0; colIndex < len(row); colIndex++ {
+			expected, err := strconv.Atoi(strconv.Itoa(rowIndex) + strconv.Itoa(colIndex))
+			if err != nil {
+				t.Error("Expected err to be not null, but got ", err)
+			}
+			if multi[rowIndex][colIndex] != expected {
+				t.Errorf("Expected %d, got %d", expected, multi[rowIndex][colIndex])
+			}
 		}
 	}
 }
