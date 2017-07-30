8f6f8ebffdc0e98b9e627bba139ed086895115d8
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-07-30 23:54:30 +0530

    Adds dope vector and dynamic arrays

---
+++ arrays/dope_vector.go
@@ -0,0 +1,5 @@
+package arrays
+
+// In computer programming, a dope vector is a data structure used to hold information about a data object,[1] e.g. an array, especially its memory layout.
+
+// A dope vector typically contains information about the type of array element, rank of an array, the extents of an array (range of indices: zero-based, one-based, or in some languages from any integer (even negative) to any integer), and the stride of an array as well as a pointer to the block in memory containing the array elements.
---
+++ arrays/dynamic_array.go
@@ -0,0 +1,6 @@
+package arrays
+
+// In computer science, a dynamic array, growable array, resizable array, dynamic table, mutable array, or array list is a random access, variable-size list data structure that allows elements to be added or removed.
+
+// Go uses slices to provide dynamic array implementation
+var dynArr []int
