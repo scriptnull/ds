b47d0161de3168b00741bd826e15b2593a06c169
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-07-30 23:29:53 +0530

    adds circular buffer and control table

---
+++ arrays/circular_buffer.go
@@ -0,0 +1,6 @@
+package arrays
+
+// A circular buffer, circular queue, cyclic buffer or ring buffer is a data structure that uses a single, fixed-size buffer as if it were connected end-to-end.
+// This structure lends itself easily to buffering data streams.
+
+// Please check adt/queue.go -> ArrQueue for an implementation of this.
---
+++ arrays/control_table.go
@@ -0,0 +1,7 @@
+package arrays
+
+// Control tables are tables that control the control flow or play a major part in program control.
+// There are no rigid rules about the structure or content of a control table—its qualifying attribute is its ability to direct control flow in some way through "execution" by a processor or interpreter.
+
+// Control tables are mostly implemented as  system level data structures, while implementing a new programming language etc.
+// In order to implement a control table, we need to implement a interpreter also. Then only the system can know how to use the control table
